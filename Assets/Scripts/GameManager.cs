0364986a51d76ce66faa8a7ad9e81d6c0aa89e08
Šimon Jelínek <simonjelinek2005@gmail.com>
2021-07-11 12:50:57 +0200

    Added basic project structure

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void Awake()
+    {
+        App.gameManager = this;
+    }
+
+    void Start()
+    {
+        App.screenManager.Show<MenuScreen>();
+        App.screenManager.Hide<InGameScreen>();
+    }
+
+    public void StartGame()
+    {
+        App.screenManager.Show<InGameScreen>();
+        App.screenManager.Hide<MenuScreen>();
+    }
+
+    public void ReturnToMenu()
+    {
+        App.screenManager.Show<MenuScreen>();
+        App.screenManager.Hide<InGameScreen>();
+    }
+}
---
+++ Assets/Scripts/UI/ScreenManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+    public ScreenBase[] screens;
+
+    void Awake()
+    {
+        screens = GetComponentsInChildren<ScreenBase>(true);
+        App.screenManager = this;
+    }
+
+    public virtual void Show<T>()
+    {
+        foreach (ScreenBase screen in screens)
+        {
+            if (screen.GetType()==typeof(T))
+            {
+                screen.Show();
+            }
+        }
+    }
+
+    public virtual void Hide<T>()
+    {
+        foreach (ScreenBase screen in screens)
+        {
+            if (screen.GetType() == typeof(T))
+            {
+                screen.Hide();
+            }
+        }
+    }
+}
