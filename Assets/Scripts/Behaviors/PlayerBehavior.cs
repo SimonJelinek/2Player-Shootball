ecd6ff1434b77088f1f23ff6bc67fb6dfa0e9275
Šimon Jelínek <simonjelinek2005@gmail.com>
2021-07-11 14:11:59 +0200

    Added player rotation

---
+++ Assets/Scripts/Behaviors/PlayerBehavior.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public float rotSpeed;
+
+    void FixedUpdate()
+    {
+        transform.Rotate(new Vector3(0,0,-1*rotSpeed));
+    }
+}
---
+++ Assets/Scripts/Behaviors/PlayerRed.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRed : PlayerBehavior
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
