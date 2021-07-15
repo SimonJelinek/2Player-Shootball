19dd68e360eddfe7ebd3f0bea84fbfd6816d8e87
Šimon Jelínek <simonjelinek2005@gmail.com>
2021-07-15 19:50:17 +0200

    Added settings screen

--- Assets/Scripts/UI/MenuScreen.cs
+++ Assets/Scripts/UI/MenuScreen.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class MenuScreen : ScreenBase
 {
+    public void ShowSettings() 
+    {
+        App.screenManager.Show<SettingsScreen>();
+        App.screenManager.Hide<MenuScreen>();
+    }
     
     public void ShowLevels() 
     {
         App.screenManager.Show<LevelsScreen>();
+        App.screenManager.Hide<MenuScreen>();
     }
 
     public void HideLevels() 
     {
         App.screenManager.Hide<LevelsScreen>();
+        App.screenManager.Show<MenuScreen>();
     }
 
     public void QuitButton() 
---
+++ Assets/Scripts/UI/SettingsScreen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsScreen : ScreenBase
+{
+    public void ReturnToMenu() 
+    {
+        Hide();
+        App.screenManager.Show<MenuScreen>();
+    }
+}
